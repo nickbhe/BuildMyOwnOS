03ba5a0ff3383a13968b0053dae26ff797063ebf
nickbhe <nickbhe@gmail.com>
2020-12-03 06:51:55 -0500

    add split to tokens [mid work]

--- shell.c
+++ shell.c
@@ -1,18 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-void ush_loop() {
+void split_to_tokens(char *line) {
+  char *tmp = line;
+  int tokenAmount = 1;
+  char previousCharacter = NULL;
+  
+  while(*tmp) {
+    if(*tmp == ' ' && previousCharacter != ' ') {
+      tokenAmount++;
+    }
+    
+    printf("%s", *tmp);
+    previousCharacter = *tmp;
+    tmp++;
+  }
+ 
+
+  char **tokens;
+  int tokenIndex = 0;
+  
+}
+
+void shsh_loop() {
   char *line = NULL;
   int capacity = 0;
+  int tokenAmount;
+  char previousCharacter = NULL;
+  
   while(1) {
     printf("\u2660 -> ");
     getline(&line, &capacity, stdin);
-    printf(line);
+    
+    split_to_tokens(line);
   }
 }
 
 int main(int argc, char **argv) {
-  ush_loop();
+  shsh_loop();
   
   return EXIT_SUCCESS;
 }
