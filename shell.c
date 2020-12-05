13d5add72fe7ec24566413d6a2e688120cb70b6e
nickbhe <nickbhe@gmail.com>
2020-12-05 05:54:30 -0500

    Extract readLine to function to handle read errors

--- shell.c
+++ shell.c
@@ -3,9 +3,25 @@
 #include <stdio.h>
 #include <string.h>
 
+void shsh_read_line(char **returnLine) {
+  char *line = NULL;
+  size_t capacity = 0;
+
+  if (getline(&line, &capacity, stdin) == -1) {
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS); 
+    } else {
+      perror("shsh: Readline Failure");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    *returnLine = line;
+  }
+}
+
 #define TOKEN_BUFSIZE 64
 #define TOKEN_DELIM " \n\a\r\t"
-void split_to_tokens(char *line, char*** returnTokens) {
+void shsh_split_to_tokens(char *line, char ***returnTokens) {
   int buffer = TOKEN_BUFSIZE;
   char **tokens = malloc(buffer * sizeof(char*));
 
@@ -17,19 +33,19 @@ void split_to_tokens(char *line, char*** returnTokens) {
   char *saveptr;
   char *token = strtok_r(line, TOKEN_DELIM, &saveptr);
   
-  if(token == NULL) {
+  if (token == NULL) {
     *returnTokens = NULL;
   } else {
     int tokenAmount = 0;
 
-    while(token != NULL) {
+    while (token != NULL) {
       tokens[tokenAmount] = token;
       tokenAmount++;
       
-      if(tokenAmount >= TOKEN_BUFSIZE) {
+      if (tokenAmount >= TOKEN_BUFSIZE) {
         buffer += TOKEN_BUFSIZE;
         tokens = realloc(tokens, buffer * sizeof(char*));
-        if(!tokens) {
+        if (!tokens) {
           printf("shsh: Reallocation Failure");
           exit(EXIT_FAILURE);
         }
@@ -44,19 +60,18 @@ void split_to_tokens(char *line, char*** returnTokens) {
 }
 
 void shsh_loop() {
-  while(1) {
-    char *line = NULL;
-    size_t capacity = 0;
+  while (1) {
+    char *line;
     char **tokens;
 
     printf("\u2660 -> ");
-    getline(&line, &capacity, stdin);
-    split_to_tokens(line, &tokens);
+    shsh_read_line(&line);
+    shsh_split_to_tokens(line, &tokens);
     
-    if(tokens == NULL) {
+    if (tokens == NULL) {
       printf("No Input was provided");
     } else {
-      printf("%s %s %s", tokens[0], tokens[1], tokens[2]);
+      execvp(tokens[0], tokens);
     }
   }
 }
